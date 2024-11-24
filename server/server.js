7d359cec05446af121924a124b57f94d56dcd5f1
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-24 22:10:09 +0000

    feat: configure server.js
    - configure dotenv to use environment variables
    - register the routes in fastify for client access

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 import Fastify from "fastify";
+import { config } from "dotenv";
+import { footballRoutes } from "./routes/footballApiRoutes.js";
+
+config();
 
 const fastify = Fastify({ logger: true });
+
+fastify.register(footballRoutes, { prefix: 'football' });
 
 fastify.listen({ port: 8080 }, (err, address) => {
   if (err) {
