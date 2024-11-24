2c53b6889cdcad2dc2e3584141f0321b36efde66
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-24 22:13:40 +0000

    feat: configure fastify cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 import Fastify from "fastify";
 import { config } from "dotenv";
+import cors from "@fastify/cors";
 import { footballRoutes } from "./routes/footballApiRoutes.js";
 
 config();
 
 const fastify = Fastify({ logger: true });
+
+fastify.register(cors, {
+  origin: '*',
+  methods: ['GET']
+});
 
 fastify.register(footballRoutes, { prefix: 'football' });
 
