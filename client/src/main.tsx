bf6b0aa2c0cc3425edc86925272b3cc13494abd0
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-25 08:49:29 +0000

    feat: import the theme provider as the default theme (dark)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import './styles/index.css';
 import { createRoot } from 'react-dom/client';
 import { AppRoutes } from "./routes/appRoutes.js";
+import { ThemeProvider } from './components/theme-provider.js';
 
-createRoot(document.getElementById('root')!).render( <AppRoutes /> );
+createRoot(document.getElementById('root')!).render(
+  <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+    <AppRoutes />
+  </ThemeProvider>
+);
