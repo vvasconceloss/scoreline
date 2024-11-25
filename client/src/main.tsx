25a099b972922dab85ff54d617b29d29447aef9e
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-25 08:45:43 +0000

    feat: import the router and set the default css file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,5 @@
+import './styles/index.css';
 import { createRoot } from 'react-dom/client';
+import { AppRoutes } from "./routes/appRoutes.js";
 
-createRoot(document.getElementById('root')!).render();
+createRoot(document.getElementById('root')!).render( <AppRoutes /> );
