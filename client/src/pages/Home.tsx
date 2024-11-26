9c4e478b8108f63b7bed4011db3f721438eb5ee0
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-26 13:11:22 +0000

    feat: import the current day's games component into Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,22 +1,25 @@
 import { Header } from "@/components/header/header-component";
 import { AppSidebar } from "@/components/sidebar/app-siderbar";
+import { TodayMatch } from "@/components/today-match/TodayMatch";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-
 
 const Home = () => {
   return (
     <>
-    <SidebarProvider>
-      <AppSidebar />
-      <main className="w-full">
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="w-full">
           <SidebarTrigger />
-        <div className="flex items-center space-x-28">
-          <Header />
-        </div>
-      </main>
-    </SidebarProvider>
+          <div className="flex flex-col">
+            <Header />
+            <div className="flex justify-between px-10 py-5">
+              <TodayMatch />
+            </div>
+          </div>
+        </main>
+      </SidebarProvider>
     </>
-  )
-}
+  );
+};
 
 export default Home;
