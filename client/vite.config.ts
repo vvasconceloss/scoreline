bcae9d4943024a86c807e27a6fdb4c4eec75be5e
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-26 10:07:03 +0000

    feat: configure dotenv with vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,9 @@
 import path from 'path'
+import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
